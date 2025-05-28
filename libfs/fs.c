54471679d36f7e2be5c635ce22881fe8ac0ee510
Oscar Pineda <74656047+Ace9414@users.noreply.github.com>
2025-05-27 17:14:08 -0700

    transferred code for phase 1

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,21 +7,117 @@
 #include "disk.h"
 #include "fs.h"
 
-/* TODO: Phase 1 */
+#define FS_SIG "ECS150FS"
+#define FS_SIG_LEN 8
+#define FAT_EOC 0xFFFF
+
+struct __attribute__((packed)) superblock {
+    char signature[8];             // 0x00
+    uint16_t total_blk_count;         // 0x08
+    uint16_t rdir_blk;           // 0x0A
+    uint16_t data_blk;           // 0x0C
+    uint16_t data_count;           // 0x0E
+    uint8_t fat_blk_count;            // 0x10
+    uint8_t unused[BLOCK_SIZE - 16]; // 0x11
+};
+
+// Root directory entry
+struct __attribute__((packed)) root_entry {
+    char filename[FS_FILENAME_LEN];   // 0x00
+    uint32_t size;                    // 0x10
+    uint16_t first_data_index;        // 0x14
+    uint8_t unused[10];               // 0x16
+};
+
+static struct superblock sb;
+static uint16_t *fat = NULL;
+static struct root_entry root_dir[FS_FILE_MAX_COUNT];
+static int fs_mounted = 0;
 
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+	    if (fs_mounted)
+        return -1;
+
+    if (block_disk_open(diskname) < 0)
+        return -1;
+
+    if (block_read(0, &sb) < 0)
+        return -1;
+
+    if (strncmp(sb.signature, FS_SIG, FS_SIG_LEN) != 0)
+        return -1;
+
+    if (sb.total_blk_count != block_disk_count())
+        return -1;
+
+    fat = malloc(sb.fat_blk_count * BLOCK_SIZE);
+    if (!fat)
+        return -1;
+
+    for (int i = 0; i < sb.fat_blk_count; i++) {
+        if (block_read(1 + i, (uint8_t*)fat + i * BLOCK_SIZE) < 0)
+            return -1;
+    }
+
+    if (block_read(sb.rdir_blk, root_dir) < 0)
+        return -1;
+
+    fs_mounted = 1;
+    return 0;
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+	    if (!fs_mounted)
+        return -1;
+
+    if (block_write(sb.rdir_blk, root_dir) < 0)
+        return -1;
+
+    for (int i = 0; i < sb.fat_blk_count; i++) {
+        if (block_write(1 + i, (uint8_t*)fat + i * BLOCK_SIZE) < 0)
+            return -1;
+    }
+
+    free(fat);
+    fat = NULL;
+    fs_mounted = 0;
+
+    return block_disk_close();
 }
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+	   if (!fs_mounted)
+        return -1;
+
+    printf("FS Info:\n");
+    printf("total_blk_count=%d\n", sb.total_blk_count);
+    printf("fat_blk_count=%d\n", sb.fat_blk_count);
+    printf("rdir_blk=%d\n", sb.rdir_blk);
+    printf("data_blk=%d\n", sb.data_blk);
+    printf("data_blk_count=%d\n", sb.data_count);
+
+    int fat_used = 0;
+    for (int i = 0; i < sb.data_count; i++) {
+        if (fat[i] != 0) {
+            fat_used++;
+        }
+    }
+    int fat_free = sb.data_count - fat_used;
+    printf("fat_free_ratio=%d/%d\n", fat_free, sb.data_count);
+
+    int root_used = 0;
+    for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+        if (root_dir[i].filename[0] != '\0') {
+            root_used++;
+        }
+    }
+    int root_free = FS_FILE_MAX_COUNT - root_used;
+    printf("rdir_free_ratio=%d/%d\n", root_free, FS_FILE_MAX_COUNT);
+
+    return 0;
 }
 
 int fs_create(const char *filename)
